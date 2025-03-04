b25ed82378ab0d15962b5f02a2c3d3c9dbabeb7e
Daniel Collingwood <82693586+danzuep@users.noreply.github.com>
2025-03-04 22:46:30 +0800

    CacheEntry experiment

---
+++ source/OpenEventStream/Models/CacheEntry.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using OpenEventStream.Abstractions;
+using OpenEventStream.Services;
+
+namespace OpenEventStream.Models
+{
+    public sealed class CacheEntry<T> : INotifyPropertyChanged
+    {
+        private long _lastUsed;
+        private long _lastUpdated;
+        private int _accessCount;
+        private T? _value;
+        private readonly object _key;
+        private readonly Func<T> _valueFactory;
+        private readonly ITimestampProvider _timestampProvider;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public CacheEntry(object key, Func<T> valueFactory, ITimestampProvider? timestampProvider = null)
+        {
+            _key = key ?? throw new ArgumentNullException(nameof(key));
+            _valueFactory = valueFactory ?? throw new ArgumentNullException(nameof(valueFactory));
+            _timestampProvider = timestampProvider ?? new SystemUtcTicks();
+            _lastUsed = _timestampProvider.Ticks;
+        }
+
+        public DateTime LastUsed => new DateTime(_lastUsed);
+
+        public DateTime LastUpdated => new DateTime(_lastUsed);
+
+        public int AccessCount => _accessCount;
+
+        public object Key => _key;
+
+        public T? Value
+        {
+            get
+            {
+                _accessCount++;
+                _lastUsed = _timestampProvider.Ticks;
+                OnPropertyChanged(nameof(LastUsed));
+                lock (_key)
+                {
+                    return _value;
+                }
+            }
+        }
+
+        public bool TryUpdate([MaybeNullWhen(false)] out T value, TimeSpan? slidingExpiration = null)
+        {
+            var slidingLimit = _timestampProvider.Ticks - slidingExpiration?.Ticks ?? 0;
+            if (slidingLimit > _lastUpdated)
+            {
+                _lastUpdated = _timestampProvider.Ticks;
+                OnPropertyChanged(nameof(LastUpdated));
+                value = _valueFactory();
+                lock (_key)
+                {
+                    _value = value;
+                }
+                OnPropertyChanged(nameof(Value));
+                return true;
+            }
+            value = default;
+            return false;
+        }
+
+        public bool TryDispose(TimeSpan? slidingExpiration = null)
+        {
+            var slidingLimit = _timestampProvider.Ticks - slidingExpiration?.Ticks ?? 0;
+            if (slidingLimit > _lastUsed)
+            {
+                lock (_key)
+                {
+                    _value = default;
+                }
+                OnPropertyChanged(nameof(Value));
+                return true;
+            }
+            return false;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ source/OpenEventStream/Services/AutoRefreshCacheService.cs
@@ -0,0 +1,97 @@
+﻿namespace OpenEventStream.Services;
+
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using OpenEventStream.Abstractions;
+using OpenEventStream.Models;
+
+public sealed class AutoRefreshCacheService<T> : IEnumerable<CacheEntry<T>>
+{
+    private long _lastExpired;
+    private long _lastUpdated;
+    private readonly Timer _updater;
+    private readonly CacheOptions _cacheOptions;
+    private readonly ConcurrentQueue<CacheEntry<T>> _cache = new();
+
+    private readonly ITimestampProvider _timestampProvider;
+
+    public AutoRefreshCacheService(CacheOptions? cacheOptions = null, ITimestampProvider? timestampProvider = null)
+    {
+        _timestampProvider = timestampProvider ?? new SystemUtcTicks();
+        _cacheOptions = cacheOptions ?? new CacheOptions();
+        _lastExpired = _timestampProvider.Ticks;
+        _lastUpdated = _timestampProvider.Ticks;
+        _updater = new Timer(Scan, null, _cacheOptions.Timeout, _cacheOptions.Timeout);
+    }
+
+    public int Count => _cache.Count;
+
+    public T? Add(object key, Func<T> valueFactory)
+    {
+        ArgumentNullException.ThrowIfNull(key, nameof(key));
+        var cacheEntry = new CacheEntry<T>(key, valueFactory, _timestampProvider);
+        _cache.Enqueue(cacheEntry);
+        return cacheEntry.Value;
+    }
+
+    public void UpdateInterval(TimeSpan interval)
+    {
+        _updater.Change(interval, interval);
+    }
+
+    private void Scan(object? state)
+    {
+        var expired = RemoveExpired().ToList();
+        var updated = RefreshCache().ToList();
+    }
+
+    private IEnumerable<CacheEntry<T>> RefreshCache()
+    {
+        var slidingLimit = _timestampProvider.Ticks - _cacheOptions.Timeout.Ticks;
+        if (slidingLimit > _lastUpdated)
+        {
+            _lastUpdated = _timestampProvider.Ticks;
+            while (_cache.TryPeek(out var oldest) &&
+                oldest.TryUpdate(out var cacheEntry, _cacheOptions.Timeout))
+            {
+                _cache.TryDequeue(out _);
+                Add(oldest.Key, () => cacheEntry);
+                yield return oldest;
+            }
+        }
+    }
+
+    private IEnumerable<CacheEntry<T>> RemoveExpired()
+    {
+        var slidingLimit = _timestampProvider.Ticks - _cacheOptions.Expiry.Ticks;
+        if (slidingLimit > _lastExpired)
+        {
+            _lastExpired = _timestampProvider.Ticks;
+            while (_cache.TryPeek(out var oldest) &&
+                oldest.TryDispose(_cacheOptions.Expiry))
+            {
+                _cache.TryDequeue(out _);
+                yield return oldest;
+            }
+        }
+    }
+
+    public IEnumerator<CacheEntry<T>> GetEnumerator()
+    {
+        foreach (var record in _cache)
+        {
+            yield return record;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Dispose()
+    {
+        _cache.Clear();
+    }
+}
